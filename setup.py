23602a5249edd086f70e9ceca84823a5153dac3d
Colm Talbot <colm.talbot@monash.edu>
2019-02-06 16:03:58 +1100

    add regular likelihood

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup
 setup(
     name='population',
     version='0.1.1',
-    packages=['population'],
-    package_dir={'population': '.'}
+    packages=['gwpopulation'],
+    package_dir={'gwpopulation': '.'}
 )

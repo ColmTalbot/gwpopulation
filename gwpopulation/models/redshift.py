de6f61e3e571e229fa88737890546f2b777cfa33
Colm Talbot <colm.talbot@monash.edu>
2020-02-19 10:15:46 +0000

    Redshift models development (#17)
    * add more general redshift models
    
    * fix redshift models

--- gwpopulation/models/redshift.py
+++ gwpopulation/models/redshift.py
@@ -1,4 +1,4 @@
-from .. cupy_utils import to_numpy, trapz, xp
+from ..cupy_utils import to_numpy, trapz, xp
 from ..utils import powerlaw
 
 import numpy as np
@@ -6,40 +6,78 @@ import numpy as np
 from astropy.cosmology import Planck15
 
 
-class PowerLawRedshift(object):
+class _Redshift(object):
     """
-    Redshift model from Fishbach+ https://arxiv.org/abs/1805.10270
-    Note that this is deliberately off by a factor of dVc/dz
+    Base class for models which include a term like dVc/dz / (1 + z)
     """
 
-    def __init__(self):
-        self.zs_ = np.linspace(1e-3, 1, 1000)
+    def __init__(self, z_max=1):
+        self.z_max = z_max
+        self.zs_ = np.linspace(1e-3, z_max, 1000)
         self.zs = xp.asarray(self.zs_)
-        self.dvc_dz_ = (
-            Planck15.differential_comoving_volume(self.zs_).value * 4 * np.pi)
+        self.dvc_dz_ = Planck15.differential_comoving_volume(self.zs_).value * 4 * np.pi
         self.dvc_dz = xp.asarray(self.dvc_dz_)
         self.cached_dvc_dz = None
 
-    def __call__(self, dataset, lamb):
-        p_z = powerlaw(1 + dataset['redshift'], alpha=(lamb - 1),
-                       high=(1 + self.zs_[-1]), low=1)
+    def _cache_dvc_dz(self, redshifts):
+        self.cached_dvc_dz = xp.asarray(
+            np.interp(to_numpy(redshifts), self.zs_, self.dvc_dz_)
+        )
+
+    def normalisation(self, psi_of_z):
+        norm = trapz(psi_of_z * self.dvc_dz / (1 + self.zs), self.zs)
+        return norm
+
+    def probabilty_from_psi_of_z(self, psi_of_z, dataset):
+        normalisation = self.normalisation(psi_of_z)
+        p_z = psi_of_z / (1 + dataset["redshift"]) / normalisation
         try:
             p_z *= self.cached_dvc_dz
         except (TypeError, ValueError):
-            self._cache_dvc_dz(dataset['redshift'])
+            self._cache_dvc_dz(dataset["redshift"])
             p_z *= self.cached_dvc_dz
-        p_z /= self.normalisation(lamb)
         return p_z
 
-    def normalisation(self, lamb):
-        p_z_ = powerlaw(1 + self.zs, alpha=(lamb - 1),
-                        high=(1 + self.zs_[-1]), low=1)
-        norm = trapz(p_z_ * self.dvc_dz, self.zs)
-        return norm
 
-    def _cache_dvc_dz(self, redshifts):
-        self.cached_dvc_dz = xp.asarray(np.interp(
-            to_numpy(redshifts), self.zs_, self.dvc_dz_))
+class PowerLawRedshift(_Redshift):
+    """
+    Redshift model from Fishbach+ https://arxiv.org/abs/1805.10270
+
+    Parameters
+    ----------
+    z_max: float, optional
+        The maximum redshift allowed.
+    """
+
+    def __call__(self, dataset, lamb):
+        psi_of_z = powerlaw(
+            1 + dataset["redshift"], alpha=lamb, high=1 + self.z_max, low=1
+        )
+        return self.probabilty_from_psi_of_z(psi_of_z=psi_of_z, dataset=dataset)
+
+
+class MaduaDickinsonRedshift(_Redshift):
+    """
+    Redshift model from Fishbach+ https://arxiv.org/abs/1805.10270 (33)
+
+    Parameters
+    ----------
+    a_z: float
+        Slope of the distribution at low redshift
+    b_z: float
+        Slope of the distribution at high redshift
+    z_peak: float
+        Redshift at which the distribution peaks.
+    z_max: float, optional
+        The maximum redshift allowed.
+    """
+
+    def __call__(self, dataset, a_z, b_z, z_peak):
+        psi_of_z = powerlaw(1 + self.zs, alpha=a_z, high=1 + self.z_max, low=1)
+        psi_of_z /= 1 + a_z / (b_z - a_z) / (1 + z_peak) ** b_z * powerlaw(
+            1 + self.zs, alpha=b_z, high=1 + self.z_max, low=1
+        )
+        return self.probabilty_from_psi_of_z(psi_of_z=psi_of_z, dataset=dataset)
 
 
 power_law_redshift = PowerLawRedshift()
--- test/redshift_test.py
+++ test/redshift_test.py
@@ -2,23 +2,35 @@ from __future__ import division
 
 import unittest
 
-import numpy as np
+from bilby.core.prior import PriorDict, Uniform
 
 from gwpopulation.models import redshift
 from gwpopulation.cupy_utils import trapz, xp
 
 
-class TestFHFRedshift(unittest.TestCase):
-
+class TestRedshift(unittest.TestCase):
     def setUp(self):
         self.zs = xp.linspace(1e-3, 1, 1000)
         self.test_data = dict(redshift=self.zs)
         self.n_test = 100
 
-    def test_fhf_normalised(self):
+    def _run_model(self, model, priors):
         norms = list()
         for _ in range(self.n_test):
-            lamb = np.random.uniform(-15, 15)
-            p_z = redshift.power_law_redshift(self.test_data, lamb)
+            p_z = model(self.test_data, **priors.sample())
             norms.append(trapz(p_z, self.zs))
         self.assertAlmostEqual(xp.max(xp.abs(xp.asarray(norms) - 1)), 0.0)
+
+    def test_powerlaw_normalised(self):
+        model = redshift.PowerLawRedshift()
+        priors = PriorDict()
+        priors["lamb"] = Uniform(-15, 15)
+        self._run_model(model=model, priors=priors)
+
+    def test_madau_dickinson_normalised(self):
+        model = redshift.MaduaDickinsonRedshift()
+        priors = PriorDict()
+        priors["a_z"] = Uniform(-15, 15)
+        priors["b_z"] = Uniform(-15, 15)
+        priors["z_peak"] = Uniform(0, model.z_max)
+        self._run_model(model=model, priors=priors)

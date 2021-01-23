debf4074fc659c198538ddf5eae37dd5f17c4894
Amanda Farah <amandamfarah@gmail.com>
2021-01-23 12:24:07 -0600

    more minor style changes

--- gwpopulation/hyperpe.py
+++ gwpopulation/hyperpe.py
@@ -93,7 +93,7 @@ class HyperparameterLikelihood(Likelihood):
         if isinstance(hyper_prior, types.FunctionType):
             hyper_prior = Model([hyper_prior])
         elif not (
-            hasattr(hyper_prior, "parameters") 
+            hasattr(hyper_prior, "parameters")
             and callable(getattr(hyper_prior, "prob"))
         ):
             raise AttributeError(

7a6fcc175fe989e432c9c3dc7d001183e62d344d
Amanda Mirna Farah <amanda.farah@ligo.org>
2021-08-19 16:07:27 -0700

    enfore truncation in mass distribution at 1 and 100 solar masses to match with injections

--- gwpopulation/models/mass.py
+++ gwpopulation/models/mass.py
@@ -58,7 +58,7 @@ def matter_matters(mass, A, NSmin, NSmax, BHmin, BHmax,
         depth of the dip between NSmax and BHmin (A).
     """
     mbreak=BHmin
-    logprob = xp.where(mass >= NSmin, 
+    logprob = xp.where((mass >= 1)*(mass <= 100), 
                        -xp.log(1 + (NSmin/mass)**n0) \
                        + xp.log(1.0 - A/((1 + (NSmax/mass)**n1) * (1 + (mass/BHmin)**n2))) \
                        - xp.log(1 + (mass/BHmax)**n3)\

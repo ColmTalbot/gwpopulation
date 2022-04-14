51aa69d28d00003208a41d015b2a7e5b01c9c9dd
jacobgolomb <87513955+jacobgolomb@users.noreply.github.com>
2022-04-14 12:09:42 -0700

    Update interped.py

--- gwpopulation/models/interped.py
+++ gwpopulation/models/interped.py
@@ -95,5 +95,5 @@ class InterpolatedNoBaseModelIdentical(object):
                 
         if not nodes == self.nodes:
             print(f'Different number of nodes! Using {nodes} nodes instead of {self.nodes}')
-            self.__init__(self.parameters, self.min, self.max, nodes, self.kinds)
+            self.__init__(self.parameters, self.min, self.max, nodes, self.kind)
 

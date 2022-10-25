34df60792483ae19dbdf97a05bf7337f430b6b7b
Colm Talbot <talbotcolm@gmail.com>
2022-10-25 15:51:42 -0400

    MAINT: switch to setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -1,47 +1,8 @@
 #!/usr/bin/env python
 
 import os
-import subprocess
 
 from setuptools import find_packages, setup
-
-
-def write_version_file(version):
-    """Writes a file with version information to be used at run time
-
-    Parameters
-    ----------
-    version: str
-        A string containing the current version information
-
-    Returns
-    -------
-    version_file: str
-        A path to the version file
-
-    """
-    try:
-        git_log = subprocess.check_output(
-            ["git", "log", "-1", "--pretty=%h %ai"]
-        ).decode("utf-8")
-        git_diff = (
-            subprocess.check_output(["git", "diff", "."])
-            + subprocess.check_output(["git", "diff", "--cached", "."])
-        ).decode("utf-8")
-        if git_diff == "":
-            git_status = "(CLEAN) " + git_log
-        else:
-            git_status = "(UNCLEAN) " + git_log
-    except Exception as e:
-        print(f"Unable to obtain git version information, exception: {e}")
-        git_status = ""
-
-    version_file = ".version"
-    if os.path.isfile(version_file) is False:
-        with open("gwpopulation/" + version_file, "w+") as f:
-            f.write(f"{version}: {git_status}")
-
-    return version_file
 
 
 def get_long_description():
@@ -59,8 +20,6 @@ def readfile(filename):
     return filecontents
 
 
-VERSION = "0.7.0"
-version_file = write_version_file(VERSION)
 long_description = get_long_description()
 
 with open("requirements.txt", "r") as ff:
@@ -74,10 +33,8 @@ setup(
     author="Colm Talbot",
     author_email="talbotcolm@gmail.com",
     license="MIT",
-    version=VERSION,
     packages=find_packages(exclude=["test", "venv", "priors"]),
     package_dir={"gwpopulation": "gwpopulation"},
-    package_data={"gwpopulation": [version_file]},
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.8",

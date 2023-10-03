93d11361394b989b4d5969030e9caa35fa6375c9
ColmTalbot <ColmTalbot@users.noreply.github.com>
2023-10-03 15:40:13 +0000

    deploy: fd07a67de519f6b22cbf5580fa380b1c8181a953

--- test/example_test.py
+++ test/example_test.py
@@ -1,14 +1,14 @@
 import glob
 
 import bilby
-import numpy as np
 import pandas as pd
 
 import gwpopulation
 
 
 def test_likelihood_evaluation():
-    np.random.seed(10)
+    bilby.core.utils.random.seed(10)
+    rng = bilby.core.utils.random.rng
 
     model = bilby.hyper.model.Model(
         [
@@ -38,12 +38,10 @@ def test_likelihood_evaluation():
         prior=(1, 1),
     )
     posteriors = [
-        pd.DataFrame(
-            {key: np.random.uniform(*bound, 100) for key, bound in bounds.items()}
-        )
+        pd.DataFrame({key: rng.uniform(*bound, 100) for key, bound in bounds.items()})
         for _ in range(10)
     ]
-    vt_data = {key: np.random.uniform(*bound, 10000) for key, bound in bounds.items()}
+    vt_data = {key: rng.uniform(*bound, 10000) for key, bound in bounds.items()}
 
     selection = gwpopulation.vt.ResamplingVT(vt_model, vt_data, len(posteriors))
 
@@ -54,7 +52,7 @@ def test_likelihood_evaluation():
     priors = bilby.core.prior.PriorDict("priors/bbh_population.prior")
 
     likelihood.parameters.update(priors.sample())
-    assert abs(likelihood.log_likelihood_ratio() - 0.06141098844907589) < 0.01
+    assert abs(likelihood.log_likelihood_ratio() + 1.810695) < 0.01
 
 
 def test_prior_files_load():

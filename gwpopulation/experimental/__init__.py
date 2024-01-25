70b3438a1d0446ed3f923308f6966253d7e85e0e
Colm Talbot <colm.talbot@ligo.org>
2024-01-25 07:46:33 -0800

    DEV: add experimental code for numpyro/JAX JIT

---
+++ gwpopulation/experimental/__init__.py
@@ -0,0 +1,5 @@
+"""
+This subpackage contains experimental new features and models that do not have a finalized API.
+
+Any code in this subpackage is subject to change without warning in subsequent versions.
+"""
